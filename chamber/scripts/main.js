24a53d3c1361454e5c176487b3ddefa45526f890
alicegracee <vol23001@byui.edu>
2024-03-23 23:58:48 +0000

    w02

---
+++ chamber/scripts/main.js
@@ -0,0 +1,15 @@
+// footer 
+// current year
+document.querySelector('#year').textContent = new Date().getFullYear();
+
+// last Modified
+document.querySelector('#lastModified').textContent = new Date(document.lastModified).toUTCString();
+
+// responsive navigation 
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
