be7cac569a3df1e9f0cb6042f24b00725ffc42b2
alicegracee <vol23001@byui.edu>
2024-04-05 15:00:18 +0100

    w04

---
+++ scripts/form.js
@@ -0,0 +1,35 @@
+/* check if passwords match */
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+const message = document.querySelector("#form-message");
+
+confirmPassword.addEventListener("focusout", checkSame);
+
+function checkSame() {
+    if (password.value !== confirmPassword.value) {
+        message.textContent = "❗Passwords DO NOT MATCH!";
+        message.style.visibility = "show";
+        confirmPassword.style.backgroundColor = "#fff0f3";
+        password.style.backgroundColor = "#fff0f3";
+        confirmPassword.value = "";
+        password.value = "";
+        password.focus();
+    } else {
+        message.style.display = "none";
+        password.style.backgroundColor = "#7fffd4";
+        password.style.color = "#000";
+        confirmPassword.style.backgroundColor = "#7fffd4";
+        confirmPassword.style.color = "#000";
+    }
+}
+
+/* rating range */
+const rangevalue = document.getElementById("range-value");
+const range = document.getElementById("rating");
+
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
