7b03576d076705efffb4e9cc2e31f7c2d8c27a20
alicegracee <vol23001@byui.edu>
2024-04-10 14:20:02 +0100

    w05

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,63 @@
+const url = 'https://alicegracee.github.io/wdd230/chamber/data/members.json';
+
+/* const url = 'data/members.json'; */
+
+const cards = document.querySelector('#cards');
+
+async function getMemberData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayMembers(data.members);
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        let companyLogo = document.createElement('img');
+        let companyName = document.createElement('h3');
+        let membershipLevel = document.createElement('p');
+        let companyAddress = document.createElement('p');
+        let companyTelephone = document.createElement('p');
+        let companyWebsite = document.createElement('p');
+
+        companyLogo.setAttribute('src', member.img);
+        companyLogo.setAttribute('alt', member.alt);
+        companyLogo.setAttribute('loading', 'lazy');
+        companyLogo.setAttribute('width', '250');
+        companyLogo.setAttribute('height', '300');
+
+        companyName.textContent = `${member.name}`;
+
+        membershipLevel.textContent = `Membership level: ${member.membership}`;
+        companyAddress.textContent = member.address;
+        companyTelephone.textContent = member.phone;
+        companyWebsite.textContent = member.url;
+
+        card.appendChild(companyName);
+        card.appendChild(companyLogo);
+        card.appendChild(companyAddress);
+        card.appendChild(companyTelephone);
+        card.appendChild(companyWebsite);
+        card.appendChild(membershipLevel);
+
+        cards.appendChild(card);
+    });
+}
+
+getMemberData();
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".selector");
+
+gridbutton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
